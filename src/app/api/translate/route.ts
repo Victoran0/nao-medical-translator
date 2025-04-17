d187aca1c92f8ae0f16d826bb53f4c1919f7e879
Victoran0 <victoranosunkvic@gmail.com>
2025-04-17 01:25:11 +0100

    Create and export POST function for /api/translate; to get and return the translation from the llm

---
+++ src/app/api/translate/route.ts
@@ -0,0 +1,19 @@
+import { translator } from "./translator"
+
+
+export async function POST(req: Request) {
+    const {messages, outputLang} = await req.json()
+    const latestPrompt = messages[messages.length - 1].content
+    console.log("The user;s request: ", latestPrompt)
+    console.log("The outputLang: ", outputLang)
+    try {
+        const response = await translator(latestPrompt, outputLang)
+        console.log("Translator Response: ", response)
+        return new Response(JSON.stringify(response), {status: 200})
+        
+    } catch (error: any) {
+        console.error("The backend error: ", error)
+        return new Response(JSON.stringify(error?.response), {status: 500})
+    }
+
+}
