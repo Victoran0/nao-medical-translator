4b0677331fca1a3f391d982f0035f4018f043972
Victoran0 <victoranosunkvic@gmail.com>
2025-04-17 01:22:16 +0100

    Add toaster, header, footer and TranslatorStoreProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import { TranslatorStoreProvider } from "./providers/translator-store-provider";
+import Footer from "@/components/ui/Footer";
+import Navbar from "@/components/ui/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TranslatorStoreProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </TranslatorStoreProvider>
+        <Toaster />
       </body>
     </html>
   );
