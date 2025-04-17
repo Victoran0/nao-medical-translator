463f0bfbdf83ffa07b844854db1ec140ba7c484f
Victoran0 <victoranosunkvic@gmail.com>
2025-04-17 01:29:10 +0100

    Create Select output Language component

---
+++ src/app/translate/SelectOutputLang.tsx
@@ -0,0 +1,43 @@
+"use client"
+import * as React from "react"
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useTranslatorStore } from "../providers/translator-store-provider";
+
+const SelectOutputLang = () => {
+    const {outputLang, manageLang} = useTranslatorStore((state) => state,);
+
+    return (
+        <Select
+            onValueChange={(value) => {
+                manageLang('outputLang', value)
+            }}
+            defaultValue={outputLang}
+        >
+        <SelectTrigger className="text-center w-32 bdBox font-[600] max-[700px]:w-28 text-[12px] cursor-pointer">
+            <SelectValue placeholder="Output Lang" />
+        </SelectTrigger>
+        <SelectContent>
+            <SelectGroup>
+            <SelectLabel>Languages</SelectLabel>
+            <SelectItem value="en-US">English</SelectItem>
+            <SelectItem value="fr-FR">French</SelectItem>
+            <SelectItem value="es-ES">Spanish</SelectItem>
+            <SelectItem value="de-DE">German</SelectItem>
+            <SelectItem value="it-IT">Italian</SelectItem>
+            <SelectItem value="pt-PT">Portuguese</SelectItem>
+            </SelectGroup>
+        </SelectContent>
+        </Select>
+    )
+};
+
+export default SelectOutputLang;
